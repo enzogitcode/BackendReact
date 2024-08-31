c1d53f1c5f4da3c252b4cd7a1be4a47a9cbb2a16
enzogit <enzo_91_10@hotmail.com>
2024-08-31 01:45:08 -0300

    ticket

--- react_backend_Final/src/components/Carts/EditItemCart.jsx
+++ react_backend_Final/src/components/Carts/EditItemCart.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 useParams
 const EditItemCart = () => {
-    const {cid, pid} = useParams()
+  const { cid, pid } = useParams()
   return (
     <div>
-{cid}, {pid}
+      {cid}, {pid}
     </div>
   )
 }
